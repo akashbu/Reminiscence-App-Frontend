212c3337fd58a1e231f9c5a069b2289df7e6e192
Akash Butala <akash.butala.365@gmail.com>
2022-05-17 18:34:21 +0530

    Minor chnages

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -90,7 +90,7 @@ const Form = ({currentId, setCurrentId}) => {
         <TextField
           name="tags"
           variant="outlined"
-          label="Tags"
+          label="Tags (Add eg manali,snow,fun)"
           fullWidth
           value={postData.tags}
           onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',')})}
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -50,7 +50,7 @@ const Post = ({ post, setCurrentId }) => {
   }
 
   return (
-    <Card className={classes.card} raised elevation={6}>
+    <Card className={classes.card} raised elevation={10}>
     <ButtonBase
       component="span"
       name="test"
